5f3975859ca355bc8aab1611e59906320e78c3f5
oumar390 <oumarsyy2@gmail.com>
2026-04-21 12:40:25 +0000

    Change path aliases to explicit relative paths in backend for Vercel Node runtime compatibility

--- server/routers.ts
+++ server/routers.ts
@@ -1,4 +1,4 @@
-import { COOKIE_NAME } from "@shared/const";
+import { COOKIE_NAME } from "../shared/const.js";
 import { getSessionCookieOptions } from "./_core/cookies.js";
 import { enforceRateLimit } from "./_core/rateLimit.js";
 import { systemRouter } from "./_core/systemRouter.js";
